a5dee569de0bd684cbe02bf2f568971525b4784a
Guada <guadamiranda@justiciacordoba.gob.ar>
2023-03-16 13:55:40 -0300

    Doing Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import '../../Styles/navbar.scss'
+import { IoLogoOctocat } from 'react-icons/io';
+import { FaLinkedin, FaFacebookSquare} from 'react-icons/fa'
 
 export const Navbar = () => {
     return(
         <div id='navbar' className="navbar">
-                Hola :D
+                <div id='navbar_left-content' className='navbar__left-content'><IoLogoOctocat/></div> 
+                <div id='navbar_center-content' className='navbar__center-content'>
+                    <div>ABOUT ME</div> 
+                    <div>SKILLS</div> 
+                    <div>PROJECTS</div>
+                    <div>CONTACT</div> 
+                </div>
+                <div id='navbar_right-content' className='navbar__right-content'><FaFacebookSquare className='facebook-icon'/><FaLinkedin className='linkedin-icon'/></div>
         </div>
     )
 }
