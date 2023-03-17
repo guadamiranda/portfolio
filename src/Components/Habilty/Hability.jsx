b788fbfc849dde6e76f50f2d4c5209e1702ed937
guadyrukia <guadalupe.miranda@ymail.com>
2023-03-17 16:50:21 -0300

    Finished skills

--- src/Components/Habilty/Hability.jsx
+++ src/Components/Habilty/Hability.jsx
@@ -1,9 +1,30 @@
 import '../../Styles/hability.scss'
 
 export const Hability = (props) => {
-    return(
-        <div className="hability">
+    const habilities = props.habilities
 
-        </div>
+    return(
+        <>
+            {habilities.map((hability) => 
+            <div className='hability'>
+                <div className='hability__container-name-icon-percentage'>
+                    <div className='hability__container-name-icon'>
+                        <span className='hability__iconHabilities'>{hability.icon}</span>
+                        <span className='hability__nameHabilities'>{hability.name}</span>
+                    </div>
+                    <div className='hability__container-percentage'>
+                        <span><b>{hability.percentage}%</b></span>
+                    </div>
+                    
+                </div>
+                    
+                <div className='hability__container-percentage-bar'>
+                    <div className='hability__percentage-bar'>
+                        <div className='porcentage-bar-pink' style={{"width": hability.percentage + '%'}} ></div>
+                    </div>
+                </div>
+            </div>)}
+            
+        </>
     )
 }
--- src/Components/SkillsCard/SkillsCard.jsx
+++ src/Components/SkillsCard/SkillsCard.jsx
@@ -1,9 +1,23 @@
 import '../../Styles/skillsCard.scss'
+import { Hability } from '../Habilty/Hability'
+import { IoLogoHtml5, IoLogoCss3, IoLogoJavascript, IoLogoAngular, IoLogoNodejs } from 'react-icons/io';
+import { GrReactjs } from 'react-icons/gr';
+import { RiVuejsFill } from 'react-icons/ri';
+import { SiMongodb } from 'react-icons/si'
 
 export const SkillsCard = () => {
+
+    const habilities = [{name:"HTML", icon:<IoLogoHtml5 />, percentage: 85},
+                        {name:"CSS", icon:<IoLogoCss3 />, percentage: 80},
+                        {name:"JavaScript", icon:<IoLogoJavascript />, percentage: 80},
+                        {name:"React", icon:<GrReactjs />, percentage: 75},
+                        {name: "Angular", icon:<IoLogoAngular/>, percentage: 40},
+                        {name: "VueJS", icon:<RiVuejsFill/>, percentage: 20},
+                        {name: "MongoDB", icon:<SiMongodb/>, percentage: 20},
+                        {name: "NodeJS", icon:<IoLogoNodejs/>, percentage: 20}]
     return(
         <div className='skillsCard'>
-            Hols
+            <Hability habilities={habilities}/>
         </div>
     )
 }
